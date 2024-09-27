4d32378c1b184c2e4cc95115fdc7fad424a8c1f7
Agsergio04 <saragar509@g.educaand.es>
2024-09-27 21:01:07 +0200

    Actualizacion 27/09/2024 21:01

--- src/ej06.py
+++ src/ej06.py
@@ -1,8 +1,7 @@
 importe_producto =float(input("Introduce el precio de un producto sin iva: "))
 
 importe_con_iva = importe_producto * 1.10
-importe_producto = str(importe_producto)
-importe_con_iva = str(importe_con_iva)
 
-print("El importe de iva aplicado es " + importe_con_iva + " y el importe sin iva es de: " + importe_producto)
+
+print("El importe de iva aplicado es ",importe_con_iva," y el importe sin iva es de: ",importe_producto)
 
