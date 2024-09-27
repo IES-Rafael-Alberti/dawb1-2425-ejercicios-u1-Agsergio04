fa5f921cf012a69699cc91b055ad4798070910b7
Agsergio04 <saragar509@g.educaand.es>
2024-09-27 20:53:38 +0200

    Ejercicios del 1 al 22

---
+++ src/ej09.py
@@ -0,0 +1,5 @@
+for i in range(3):
+    i = int(input("Introduzca un numero: "))
+    i = i + i - 1
+    
+print("La suma de los numeros es : ",i)
--- src/ej20.py
+++ src/ej20.py
@@ -1,2 +1,8 @@
-telefono = input("Introduce un número de teléfono en el formato +34: ")
 
+telefono = input("Introduce un número de teléfono en el formato +34-número-extensión: ")
+
+if telefono.startswith("+34-") and '-' in telefono[4:]:
+    numero_partido = telefono.split('-')
+    numero = numero_partido[1]
+    
+print("Número de teléfono sin prefijo y extensión:", numero)
