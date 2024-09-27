733d66363ca83f1000c22b32c4fa3b720517e9ee
Agsergio04 <saragar509@g.educaand.es>
2024-09-27 20:56:09 +0200

    Ejercicios del 1 al 21

--- src/ej15.py
+++ src/ej15.py
@@ -1,6 +1,16 @@
 cantidad_banco = float(input("Introduce la cantidad de dinero en el banco: "))
 
-for i in range(3):
-    anio = i
-    calculo_interes = cantidad_banco * (1 + anio)
-    print("El interes en primer año es de: ",round(calculo_interes,1))
+anio = 1
+calculo_interes = cantidad_banco * (1 + anio)
+print("El interes en primer año es de: ",round(calculo_interes,1))
+
+
+anio = 2
+calculo_interes = cantidad_banco * (1 + anio)
+print("El interes en primer año es de: ",round(calculo_interes,1))
+
+
+anio = 3
+calculo_interes = cantidad_banco * (1 + anio)
+print("El interes en primer año es de: ",round(calculo_interes,1))
+
