13d616f0ff3e79ad636dcc6633f209d8419fc3f2
Agsergio04 <saragar509@g.educaand.es>
2024-10-01 16:05:49 +0200

    Relacion de ejercicios del 1 al 27

--- src/ej25.py
+++ src/ej25.py
@@ -1,3 +1,8 @@
-lista_compra = input("Introduce una lista de objetos separada por comas: ")
+fecha_nacimiento_pregunta = input("Introduce tu fecha de nacimiento separada en barras (dia/mes/anio): ")
 
-print(lista_compra.replace(',',"\n"))
+fecha_nacimiento = fecha_nacimiento_pregunta.split('/')
+dia = fecha_nacimiento[0]
+mes = fecha_nacimiento[1]
+anio = fecha_nacimiento[2]
+
+print("El dia del año es",dia,",junto a su mes",mes,"del año",anio)
