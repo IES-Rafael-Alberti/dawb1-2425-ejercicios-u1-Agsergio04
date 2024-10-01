ee51b96c8cf7c016335e0e5c61941483cbe2e913
Agsergio04 <saragar509@g.educaand.es>
2024-10-01 17:27:52 +0200

    Relacion  de ejercicios 01/10/2024

--- src/ej03.py
+++ src/ej03.py
@@ -1,6 +1,6 @@
-ancho = 17
-alto = 12.0
-""" Para cada una de las expresiones siguientes, intenta adivinar el valor de la expresión y su tipo sin ejecutarlas en el intérprete: """""
+"""Relacion de ejercicios 1 """
+""" 
+
 
 divison_primera = ancho / 2
 divison_entera = ancho // 2
@@ -8,3 +8,39 @@ division_segunda = alto / 3
 once = 1 + 2 * 5
 print(type(divison_primera),type(division_segunda),type(divison_entera),type(once))
 print("Las respuestas que pienso son: 1º 8.5,2º 8,3º 4,4º 11")
+
+"""
+"""Relacion de ejercicios 2 """
+
+
+ancho = 17
+alto = 12.0
+
+ancho / alto
+operacion1 = ancho / alto
+print("ancho / alto = ",operacion1,"y es del tipo",type(operacion1))
+
+ancho // 2
+operacion2 = ancho // 2
+print("ancho // 2 = ",operacion2,"y es del tipo",type(operacion2))
+
+ancho / 2
+operacion3 = ancho / 2
+print("ancho / 2 = ",operacion3,"y es del tipo",type(operacion3))
+
+ancho * 2
+operacion4 = ancho * 2
+print("ancho * 2 = ",operacion4,"y es del tipo",type(operacion4))
+
+ancho * alto
+operacion5 = ancho * alto
+print("ancho * alto = ",operacion5,"y es del tipo",type(operacion5))
+
+(5 + 1) * 3
+operacion6 = (5 + 1) * 3
+print("(5 + 1) * 3 = ",operacion6,"y es del tipo",type(operacion6))
+
+
+(5 + 1) / 3
+operacion7 = (5 + 1) / 3
+print("(5 + 1) / 3 = ",operacion7,"y es del tipo",type(operacion7))
