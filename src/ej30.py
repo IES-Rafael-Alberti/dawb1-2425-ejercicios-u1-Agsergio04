fde7ae4800c86c8f86b7a49d0b2f37fc76c79175
Agsergio04 <saragar509@g.educaand.es>
2024-10-10 19:53:52 +0200

    Relacion de ejercicios 19:53 10/01/2024

--- src/ej30.py
+++ src/ej30.py
@@ -1,50 +1,50 @@
 def comprobacion_menor_cero(comprobacion):
-    bandera2 = True
+    bandera = True
     
-    while bandera2:
+    while bandera:
         if comprobacion < 0:
             repetir_valor = int(input("Vuelve a introducir el valor porque este no puede ser menor a 0: "))
             if repetir_valor > 0:
                 comprobacion = repetir_valor
-                bandera2 = False
-    
-    
+                bandera = False
+        else:
+            bandera = False
+                
     return comprobacion 
 
-def crear_serie():
-    return 0
+def crear_serie(numero_inicio, incremento, total_serie):
+    inicio_serie = "SERIE =>"
+    serie_trozo = ""
+    
+    numero_actual = numero_inicio  
+
+    for i in range(total_serie):
+        if i == 0:
+            serie_trozo += str(int(numero_actual))  
+        elif i == total_serie - 1:
+            serie_trozo += "-"  
+        else:
+            serie_trozo += ".." + str(int(numero_actual))  
+
+        numero_actual += incremento  
+
+    
+    serie_trozo += str(int(numero_actual - incremento))  
+    serie_final = inicio_serie + " " + serie_trozo  
+
+    print(serie_final)
+
 
 def main():
-    cadena_numeros = ""
-    bandera = True
+    numero_inicio = float(input("Introduce un numero de inicio: "))
     
-    while bandera :
-        numero_inicio = float(input("Introduce un numero de inicio: "))
-        
-        incremento = float(input("Introduce con que cantidad quieres hacer el incremento"))
-        
-        if incremento < 0:
-            while bandera:
-                print("Error: el incremento debe ser mayor a 0,vuelve a introducirlo de nuevo")
-                
-                repetir_incremento = float(input("Introduce con que cantidad quieres hacer el incremento: "))
-                
-                if not repetir_incremento < 0:
-                    incremento = repetir_incremento
-                    bandera = False
-                    
-        total_serie =  int(input("Cuantas veces quieres que se le aplique el incremento: "))
-        
-        if  total_serie < 0:
-            while bandera:
-                print("Error: el incremento debe ser mayor a 0,vuelve a introducirlo de nuevo")
-                
-                repetir_total_serie = float(input("Introduce con que cantidad quieres hacer el incremento: "))
-                
-                if not repetir_total_serie < 0:
-                    total_serie = repetir_total_serie
-                    bandera = False
-            
+    incremento = float(input("Introduce con que valor quieres hacer el incremento: "))
+    incremento = comprobacion_menor_cero(incremento)
+    
+    total_serie = int(input("Cuantas veces quieres que se aplique el incremento: "))
+    total_serie = comprobacion_menor_cero(total_serie)
+
+    crear_serie(numero_inicio, incremento, total_serie)
         
 if __name__ == "__main__":
     main()
