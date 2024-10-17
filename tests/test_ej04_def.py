7516eccaf7aecd959dc88085b96d0523919da60a
Agsergio04 <agsergio@iesrafaelalberti.es>
2024-10-17 20:46:13 +0200

    Update test_ej04_def.py

--- tests/test_ej04_def.py
+++ tests/test_ej04_def.py
@@ -1,8 +1,7 @@
-from src.ej04_def import cambio_temperatura_celsius
+from src.ej04_def2 import cambio_temperatura_celsius
 
 def test_cambio_temperatura_celsius():
-    assert cambio_temperatura_celsius(15) == "59.00"
-    assert cambio_temperatura_celsius(77) == "168.00"
-    assert cambio_temperatura_celsius(0) == "0.00"
-    assert cambio_temperatura_celsius(82) == "179.60"
-    assert cambio_temperatura_celsius(-11) == "12.20"
+    assert cambio_temperatura_celsius(15) == 59.00
+    assert cambio_temperatura_celsius(20) == 68.00
+    assert cambio_temperatura_celsius(-11) == 12.20
+   
